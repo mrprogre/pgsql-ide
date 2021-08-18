24c4917fbb21ba5da0cb3a0372ab7b2fff413e52
AndreyE-lance <51997645+AndreyE-lance@users.noreply.github.com>
2021-08-18 15:38:19 +0500

    Update Main.java
    Немного более простая работа с config.txt и favorite.txt. В зависимости от ОС получаем путь к папке пользователя и сепаратор каталогов.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,55 +3,29 @@ import java.io.File;
 import java.io.IOException;
 
 public class Main {
-    static String directoryPath = "C:\\Users\\Public\\Documents\\Pides\\";
+    static final String SEPARATOR = File.separator;
+    static String directoryPath = System.getProperty("user.home")+SEPARATOR;
     static String configPath = directoryPath + "config.txt";
     static String favoritePath = directoryPath + "favorite.txt";
-    // linux
-    static String linuxPath = "./Public/pides/";
-    static String linuxConfigPath = linuxPath + "config.txt";
-    static String linuxFavoritePath = linuxPath + "favorite.txt";
-    static String OS = OSValidator.getOS();
 
     static {
-
-        if (OS.contains("win")) {
-            File directory = new File(directoryPath);
-            File configFile = new File(configPath);
-            File favoriteFile = new File(favoritePath);
-            try {
-                if (!directory.exists()) {
-                    directory.mkdirs();
-                }
-                if (!configFile.exists()) {
-                    Common common = new Common();
-                    common.copyFiles(Main.class.getResource("config.txt"), directoryPath + "config.txt");
-                }
-                if (!favoriteFile.exists()) {
-                    favoriteFile.createNewFile();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+        File directory = new File(directoryPath);
+        File configFile = new File(configPath);
+        File favoriteFile = new File(favoritePath);
+        try {
+            if (!directory.exists()) {
+                directory.mkdirs();
             }
-        } else if (OS.contains("uni")) {
-            File directory = new File(linuxPath);
-            File configFile = new File(linuxConfigPath);
-            File favoriteFile = new File(linuxFavoritePath);
-            try {
-                if (!directory.exists()) {
-                    directory.mkdirs();
-                }
-                if (!configFile.exists()) {
-                    Common common = new Common();
-                    common.copyFiles(Main.class.getResource("config.txt"), linuxPath + "config.txt");
-                }
-                if (!favoriteFile.exists()) {
-                    favoriteFile.createNewFile();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (!configFile.exists()) {
+                Common common = new Common();
+                common.copyFiles(Main.class.getResource("config.txt"), directoryPath + "config.txt");
             }
+            if (!favoriteFile.exists()) {
+                favoriteFile.createNewFile();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
     }
 
     public static void main(String[] args) {
@@ -65,5 +39,7 @@ public class Main {
         //gui.connectionBtn.doClick();
     }
 }
+
+
 
 
